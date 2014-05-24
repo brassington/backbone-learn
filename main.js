0c767b7704f86e57a62cca9d5009a4c674d423ef
J. Andrew Brassington <jabbrass@zoho.com>
2014-05-24 07:21:07 -0700

    Added backbone, and started using it

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 /**
  * Created by andrewbrassington on 5/22/14.
  */
+var Appointment = Backbone.Model.extend({});
+var appointment = new Appointment();
+var AppointmentView = Backbone.View.extend({});
