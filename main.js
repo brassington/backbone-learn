c12f5814b17527ef8c28b7ebdd83c27b1eb1926e
J. Andrew Brassington <jabbrass@zoho.com>
2014-05-25 07:31:28 -0700

    Added jquery

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 /**
  * Created by andrewbrassington on 5/22/14.
  */
-var Appointment = Backbone.Model.extend({});
+var Appointment = Backbone.Model.extend({
+  render: function() {
+    var html = '<li>' + this.model.get('title') + '</li>';
+    $(this.el).html(html);
+  }
+});
 var appointment = new Appointment();
 var AppointmentView = Backbone.View.extend({});
+var appointmentView = new AppointmentView({model: appointment});
+
