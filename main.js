e6e765650e7498f2fba36a1c3e99b4ec26018134
J. Andrew Brassington <jabbrass@zoho.com>
2014-05-29 08:56:23 -0700

    Added defaults and spacing to main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * Created by andrewbrassington on 5/22/14.
  */
+
 var Appointment = Backbone.Model.extend({
   defaults: {
     title: 'Checkup',
@@ -11,10 +12,12 @@ var Appointment = Backbone.Model.extend({
     $(this.el).html(html);
   }
 });
+
 var appointment = new Appointment({description: 'My backbone hurts'});
 appointment.set({title: 'Go to Doctor'});
 var AppointmentView = Backbone.View.extend({});
 var appointmentView = new AppointmentView({model: appointment});
+
 
 appointmentView.render();
 $('#app').html(appointmentView.el);
